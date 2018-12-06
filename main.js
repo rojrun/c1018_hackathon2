2c09136a3d1f501ad072821f180a61a828d6c56b
benlevine1 <benlevine1@gmail.com>
2018-12-05 17:54:53 -0800

    Coordinates and Map Render work

--- main.js
+++ main.js
@@ -3,19 +3,28 @@ $(document).ready(initializeApp)
 function initializeApp(){
     //$(".randomizerButton").click(modulePopUp)
     $(".randomizerButton").click(modulePopUp)
+    var coordFinder = new Coordinates("2427+Malibu+Way+Del+Mar,+CA+92014");
+    coordFinder.getCoords(initMap)
 }
-
 function modulePopUp(){
     console.log('hi')
     var moduleBox = $('<div>',{
         class: 'module', 
     })
     $('.bodyContainer').append(moduleBox)
-
 }
+function initMap(location) {
+    // The location of Uluru
+    console.log('init map location: ',location);
+    var uluru = location
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        $('#map')[0], {zoom: 6, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
 
 
-// function openModal(message){
-//     $("#myModal").modal("show");
-//     $(".modal-body > p").text(message);
-//  }
+
+
+

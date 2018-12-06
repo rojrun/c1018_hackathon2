aeddb9d447d1bb5ee1cda098a5908df4f19452db
rojrun <runroj@gmail.com>
2018-12-05 18:23:56 -0800

    php added

--- beer.js
+++ beer.js
@@ -1,21 +1,19 @@
 
-
-
-
 $(document).ready(start);
 var drink;
 function start(){
     drink = new Beer;
 }
 
+
 class Beer {
     constructor (){
-        this.getRandomBeer();
+        this.getRandomBeer = this.getRandomBeer();
     }
     getRandomBeer (){
-        var ajaxOptions = {
-            format: 'json',
-            url: 'https://sandbox-api.brewerydb.com/v2/beer/random',
+        let ajaxOptions = {
+            dataType: 'json',
+            url: 'proxies/randombeer.php',
             method:'get',
             data:{
                 key: 'b9dea19c057881357318d0bd68522753',
@@ -33,6 +31,10 @@ class Beer {
 
         $.ajax(ajaxOptions).then(function(response){
             console.log(response);
+            var beerName = response.data.name;
+            var breweryName = response.data.breweries[0].name;
+            var breweryAddress = response.data.breweries[0].locations[0].streetAddress;
+            var breweryPostalCode = response.data.breweries[0].locations[0].streetAddress.postalCode;
         });
     }
 }

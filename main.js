55b260b9e9622ccec42f01cbea1c7124a439cbef
rojrun <runroj@gmail.com>
2019-05-13 22:42:28 -0700

    change content dynamically

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 $(document).ready(initializeApp);
 
-function initializeApp(){
-    $(".randomizerButton").click(createMeal);
-    $('.newRecipe').hide();
-    $('.newBeer').hide();
-    $('.newRecipe').click(recreateMeal);
-    $('.newBeer').click(recreateBeer);
-
+function initializeApp() {
+    var randomizerButton = $("<button>").addClass("randomizerButton").click(createMeal).text("Click for Random Munchies and Beer").appendTo(".main_content");
+    // $(".randomizerButton").click(createMeal);
+    // $('.newRecipe').hide();
+    // $('.newBeer').hide();
+    // $('.newRecipe').click(recreateMeal);
+    // $('.newBeer').click(recreateBeer);
 }
 
-function createMeal(){
-    $(".title").hide();
-    $(".randomizerButton").hide();
-    $('.newRecipe').show();
-    $('.newBeer').show();
+{/* <div class="contentRecipe">
+            <div class="recipe"></div>
+            <div class="youtube"></div>
+        </div>
+        <div class="contentBeer"><strong id="beerName"></strong>
+            <div id="beerDescription"></div>
+            <div class="content map"></div>
+        </div> */}
+
+function createMeal() {
+    // $(".title").hide();
+    $(".randomizerButton").remove();
+    var newRecipeBttn = $("<button>").addClass("newRecipe").click(recreateMeal).text("New Recipe").appendTo(".main_content");
+    var contentRecipe = $("<div>").addClass("contentRecipe").appendTo(".main_content");
+    var recipe = $("<div>").addClass("recipe").appendTo(".contentRecipe");
+    var youtube = $("<div>").addClass("youtube").appendTo(".contentRecipe");
+    var newBeerBttn = $("<button>").addClass("newBeer").click(recreateBeer).text("New Beer").appendTo(".main_content");
+    // $('.newRecipe').show();
+    // $('.newBeer').show();
     var beer = new Beer();
     var youTube = new Youtube();
     var recipe = new Recipes();
     recipe.getRecipeData(youTube.getData);
 }
+
 function recreateMeal() {
     $('iframe').remove()
     var youTube = new Youtube();
@@ -47,5 +62,4 @@ function initMap(location, title) {
     marker.addListener('click', function() {
         info.open(map, marker);
       });
-
-  }
+}
